83e999a23cd3b510c8e107e042fd0956ff5555cb
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-04-18 22:36:08 -0300

    Fix Grunt error 104:38  error  Strings must use doublequote  quotes

--- tests/e2e/modules/clock_es_spec.js
+++ tests/e2e/modules/clock_es_spec.js
@@ -101,7 +101,7 @@ describe("Clock set to spanish language module", function () {
 		});
 
 		it("shows week with correct number of week of year", function() {
-			const currentWeekNumber = require('current-week-number')();
+			const currentWeekNumber = require("current-week-number")();
 			const weekToShow = "Semana " + currentWeekNumber;
 			return app.client.waitUntilWindowLoaded()
 				.getText(".clock .week").should.eventually.equal(weekToShow);
