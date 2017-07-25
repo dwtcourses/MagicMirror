02ce46fa5d243d2c4b41380ae4df337556d8685d
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2017-07-25 16:46:01 -0400

    Add more log to help debugging errors

--- js/app.js
+++ js/app.js
@@ -31,8 +31,10 @@ if (process.env.MM_PORT) {
 // The next part is here to prevent a major exception when there
 // is no internet connection. This could probable be solved better.
 process.on("uncaughtException", function (err) {
-	console.log("Whoops! There was an uncaught exception...");
-	console.error(err);
+	console.log(Utils.colors.error("Whoops! There was an uncaught exception..."));
+	console.log("Use the follow trace to get help");
+	console.error(Utils.colors.info(err.stack));
+	console.log(process.env);
 	console.log("MagicMirror will not quit, but it might be a good idea to check why this happened. Maybe no internet connection?");
 	console.log("If you think this really is an issue, please open an issue on GitHub: https://github.com/MichMich/MagicMirror/issues");
 });
